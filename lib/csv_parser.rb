05e78d6eee78d07d06d84ec6df5e6865f2e2d768
Harry Schwartz <hello@harryrschwartz.com>
2015-12-14 11:13:05 -0500

    Don't rty to read unreadable rows

--- lib/csv_parser.rb
+++ lib/csv_parser.rb
@@ -5,11 +5,17 @@ class CsvParser
 
   def primers
     CSV.foreach(tempfile, headers: true).
-      select { |row| row[0] =~ /\w+/ }.
+      select { |row| parsable_row?(row) }.
       map { |row| Primer.new(sequence: row[1], name: row[2]) }
   end
 
   private
 
   attr_reader :tempfile
+
+  def parsable_row?(row)
+    row[0] =~ /\w+/ &&
+      row[1] =~ /\w+/ &&
+      row[2] =~ /\w+/
+  end
 end
