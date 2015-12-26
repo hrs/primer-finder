325bc9a14b1273b647791143f23ff005443962c1
Harry Schwartz <hello@harryrschwartz.com>
2015-12-25 20:11:19 -0500

    Extract helper method from specs
    Extract #fill_out_search_form. Not planning to go all the way to page
    objects, but moving in that direction.

--- spec/features/users_can_search_for_primers_spec.rb
+++ spec/features/users_can_search_for_primers_spec.rb
@@ -3,21 +3,13 @@ require "rails_helper"
 feature "Users can search for primers" do
   context "CSV contains a matching primer" do
     scenario "the results page shows only the matching primer" do
-      visit "/"
+      visit root_path
 
-      fill_in(
-        "target",
-        with: "ttaaaa"
+      fill_out_search_form(
+        target: "ttaaaa",
+        container: "cacacattaaaacccccgtgtgt",
+        csv: "spec/fixtures/primers.csv"
       )
-      fill_in(
-        "target_container",
-        with: "cacacattaaaacccccgtgtgt"
-      )
-      attach_file(
-        "primer_csv",
-        File.expand_path("spec/fixtures/primers.csv")
-      )
-      click_on "Find primers!"
 
       expect(page).to have_content("primer_1")
       expect(page).not_to have_content("primer_2")
@@ -26,21 +18,13 @@ feature "Users can search for primers" do
 
   context "CSV doesn't contain a matching primer" do
     scenario "the results page reports no matches" do
-      visit "/"
+      visit root_path
 
-      fill_in(
-        "target",
-        with: "ttagga"
+      fill_out_search_form(
+        target: "ttagga",
+        container: "cacacattaggacccccgtgtgt",
+        csv: "spec/fixtures/primers.csv"
       )
-      fill_in(
-        "target_container",
-        with: "cacacattaggacccccgtgtgt"
-      )
-      attach_file(
-        "primer_csv",
-        File.expand_path("spec/fixtures/primers.csv")
-      )
-      click_on "Find primers!"
 
       expect(page).to have_content("Couldn't match any of these primers.")
     end
@@ -48,23 +32,24 @@ feature "Users can search for primers" do
 
   context "Container doesn't contain the target region" do
     scenario "the results page displays an error" do
-      visit "/"
+      visit root_path
 
-      fill_in(
-        "target",
-        with: "aaaaa"
+      fill_out_search_form(
+        target: "aaaaa",
+        container: "gggggggggggg",
+        csv: "spec/fixtures/primers.csv"
       )
-      fill_in(
-        "target_container",
-        with: "gggggggggggg"
-      )
-      attach_file(
-        "primer_csv",
-        File.expand_path("spec/fixtures/primers.csv")
-      )
-      click_on "Find primers!"
 
       expect(page).to have_content("That target doesn't seem to be in the container you provided.")
     end
+  end
+
+  private
+
+  def fill_out_search_form(container:, csv:, target:)
+    fill_in("target", with: target)
+    fill_in("target_container", with: container)
+    attach_file("primer_csv", File.expand_path(csv))
+    click_on "Find primers!"
   end
 end
